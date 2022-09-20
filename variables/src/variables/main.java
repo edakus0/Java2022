df9176aea5929778e7340e2d7ddb91b2d448074d
edakus0 <98655821+edakus0@users.noreply.github.com>
2022-09-20 21:17:06 +0300

    Add files via upload

---
+++ variables/src/variables/main.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayi = 10;
+		String mesaj= "ogrenci sayisi ";
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+	}
+
+}
