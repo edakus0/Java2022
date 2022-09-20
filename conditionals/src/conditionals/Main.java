40178d827251b76563f215f158e81e553698fb53
edakus0 <98655821+edakus0@users.noreply.github.com>
2022-09-20 21:48:21 +0300

    Add files via upload

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,20 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+			int sayi = 24;
+			if(sayi<20) {
+				System.out.println("sayi 20'den kucuktur.");
+			}else if(sayi==20){
+				System.out.println("sayi 20'ye esittir.");
+			}else {
+				System.out.println("sayi 20'den buyuktur");
+			}
+
+		
+	}
+
+}
