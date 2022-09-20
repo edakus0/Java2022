5507826b91c4f8d69f9d17ed77113e904ff0158b
edakus0 <98655821+edakus0@users.noreply.github.com>
2022-09-20 22:14:23 +0300

    Add files via upload

---
+++ asalSayi/src/asalSayi/Main.java
@@ -0,0 +1,34 @@
+package asalSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 1;
+		int remainder=number%2;
+		System.out.println(remainder);
+		boolean isPrime = true;
+		
+		if(number==1) {
+			System.out.println("sayi asal degildir");
+		}
+		if(number<1) {
+			System.out.println("gecersiz sayi");
+		}
+		
+		
+		
+		
+		for(int i =2 ; i<number;i++) {
+			if(number%i==0) {
+				isPrime=false;
+			}
+			
+		}
+		if(isPrime==true) {
+			System.out.println("Sayi asaldir.");
+		}else {
+			System.out.println("sayi asal degildir");
+		}
+	}
+
+}
