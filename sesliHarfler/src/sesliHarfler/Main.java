2b429c1e80e1d1c0675e805edc1aa7526498e9d6
edakus0 <98655821+edakus0@users.noreply.github.com>
2022-09-20 22:17:07 +0300

    Add files via upload

---
+++ sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,23 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+        char harf='y';
+		
+		switch(harf) {
+			case 'A':
+			case 'I':
+			case 'E':
+			case 'U':
+				System.out.println("sesli harfler");
+				break;
+				default:
+					System.out.println("ince sesli harf");
+			
+		}
+		
+
+	}
+
+}
