06789fba84e672f2dcc495829923fe8382244527
edakus0 <98655821+edakus0@users.noreply.github.com>
2022-09-27 00:50:42 +0300

    Add files via upload

---
+++ oop1/src/oop1/Product.java
@@ -0,0 +1,17 @@
+package oop1;
+
+public class Product {
+
+	String name;
+	double unitPrice;
+	double discount;
+	String imageUrl;
+	int unitsInStock;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
